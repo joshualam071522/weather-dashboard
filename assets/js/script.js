c7ffb4e93678784e76b4f051026755c68264952b
joshualam071522 <127908020+joshualam071522@users.noreply.github.com>
2023-05-11 18:22:16 -0700

    added conditional to check if searched city is in localstorage or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,56 +14,55 @@ function SearchFormSubmitHandler(event) {
     //* retrieves input from user
     var searchInputEl = document.querySelector('#searchInput').value.toLowerCase().trim();
 
-    //TODO add conditional to check to render results if it is already in local storage
+    function renderWeather() {
+       var savedWeather = JSON.parse(localStorage.getItem(searchInputEl));
+        cityEl.textContent = savedWeather.city;
+        temperatureEl.textContent = 'Temp: ' + savedWeather.temperature + '° Fahrenheit ';
+        windEl.textContent = 'Wind: ' + savedWeather.wind + ' mph';
+        humidityEl.textContent = 'Humidity: ' + savedWeather.humidity + '%';  
+    }
+
+    //* Added conditional to check to render results if it is already in local storage
+    if(localStorage.getItem(searchInputEl)) {
+        console.log('found city in storage!');
+        renderWeather(searchInputEl);
+    } else {
+        console.log('finding a city through API!')
+        searchWeatherApi(searchInputEl)
+    }
 
     //* fetch the weather api
-    function searchWeatherApi () {
-        weatherQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q=&APPID=3e7fe8a93b7d45df7ddde9af30d1b389';
+    function searchWeatherApi (searchInputEl) {
+        
+        var weatherQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ searchInputEl +'&APPID=3e7fe8a93b7d45df7ddde9af30d1b389&units=imperial'
+        
+        fetch(weatherQueryURL)
+            .then(function (response) {
+                if (!response.ok) {
+                    console.log("error!!!hehe")
+                }
 
-        if (searchInputEl) {
-            weatherQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q='+ searchInputEl +'&APPID=3e7fe8a93b7d45df7ddde9af30d1b389&units=imperial'
-            
-            fetch(weatherQueryURL)
-                .then(function (response) {
-                    if (!response.ok) {
-                        console.log("error!!!hehe")
-                    }
+                return response.json();
+            })
 
-                    return response.json();
-                })
+            .then (function (weatherResults) {
+                console.log(weatherResults);
 
-                .then (function (weatherResults) {
-                    console.log(weatherResults);
+                //* store weather details into localStorage
+                var weatherDetails = {
+                    city: weatherResults.name,
+                    temperature: weatherResults.main.temp,
+                    wind: weatherResults.wind.speed,
+                    humidity: weatherResults.main.humidity
+                }
 
-                    //* store weather details into localStorage
-                    var weatherDetails = {
-                        city: weatherResults.name,
-                        temperature: weatherResults.main.temp,
-                        wind: weatherResults.wind.speed,
-                        humidity: weatherResults.main.humidity
-                    }
-
-                    localStorage.setItem(searchInputEl, JSON.stringify(weatherDetails));
-                    
-                    //* Show weather details from local storage
-                    function renderWeather() {
-                        localStorage.getItem(searchInputEl);
-                        cityEl.textContent = weatherDetails.city;
-                        temperatureEl.textContent = 'Temp: ' + weatherDetails.temperature + '° Fahrenheit ';
-                        windEl.textContent = 'Wind: ' + weatherDetails.wind + ' mph';
-                        humidityEl.textContent = 'Humidity: ' + weatherDetails.humidity + '%';  
-                    }
-
-                    renderWeather(searchInputEl);
-                })  
-            
+                localStorage.setItem(searchInputEl, JSON.stringify(weatherDetails));
+                renderWeather(searchInputEl);
+            })
         }
     }
     
-    //TODO searchForecastAPI
-    searchWeatherApi(searchInputEl);
-}
 
-
+//TODO searchForecastAPI
 //TODO render forecast function
 searchFormEl.addEventListener('submit', SearchFormSubmitHandler);
